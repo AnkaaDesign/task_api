4d855fd618f804cde28a50729d611c1403c81a4c
AnkaaDesign <plotter.ankaa@gmail.com>
2023-07-24 15:32:13 -0300

    bodyparse add

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 var cors = require('cors')
-const app = express()
-app.use(cors())
+const bodyParser = require("body-parser");
 require('dotenv').config();
 
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors())
 app.use(express.json())
 
 const connectDB = require('./connectMongo')
