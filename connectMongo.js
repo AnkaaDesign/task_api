061848cab805cc0fd067d32b11c63013a5584866
AnkaaDesign <plotter.ankaa@gmail.com>
2023-07-24 12:22:59 -0300

    initialized

--- connectMongo.js
+++ connectMongo.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
-
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
-        console.log("Connect to MongoDB successfully")
-    } catch (error) {
-        console.log("Connect failed " + error.message )
-    }
+    mongoose.connect(process.env.MONGODB_CONNECT_URl, {
+
+        useNewUrlParser: true,
+
+        useUnifiedTopology: true
+
+    }, err => {
+        if (err) throw err;
+        console.log('Connected to MongoDB!!!')
+    });
 }
+
 
 module.exports = connectDB
